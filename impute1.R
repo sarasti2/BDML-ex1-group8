f6f3a06584e53f39cb8e1f43204b017ed531eaae
angelramirez63 <ramirezangel6315@gmail.com>
2025-01-22 19:23:35 -0500

    Update impute1.R

--- impute1.R
+++ impute1.R
@@ -1,1 +1,9 @@
 # Include here your code for your first chosen imputation method
+
+
+# calculating the most commun value of maxEducLevel. 
+mode_edu <- as.numeric(names(sort(table(db$maxEducLevel), decreasing = TRUE)[1]))
+
+# Imputing the missing value. 
+db <- db  %>%
+  mutate(maxEducLevel = ifelse(is.na(maxEducLevel) == TRUE, mode_edu , maxEducLevel))
